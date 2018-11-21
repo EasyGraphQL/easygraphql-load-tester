d4fad872e517d86f81db5aa92c410754961c5bc2
Alejandro Estrada <estrada9166@gmail.com>
2018-11-20 23:40:05 -0500

    Support unions (#7)

--- util/index.js
+++ util/index.js
@@ -3,20 +3,24 @@
 
 const constants = require('./constants')
 
-function getField (field, schema, deepLevel = 0) {
+function getField (field, schema, maxDeepLevel = 5, deepLevel = 0) {
   deepLevel++
-
-  if (deepLevel > 5) return
+  if (deepLevel > maxDeepLevel) return
 
   if (!schema[field.type]) {
     return field.name
   }
 
   const nestedType = schema[field.type]
+
+  if (nestedType.values.length > 0) {
+    return field.name
+  }
+
   const fields = []
   nestedType.fields.forEach(field => {
     if (schema[field.type]) {
-      fields.push(getField(field, schema, deepLevel))
+      fields.push(getField(field, schema, maxDeepLevel, deepLevel))
     } else {
       fields.push(field.name)
     }
@@ -26,30 +30,68 @@ function getField (field, schema, deepLevel = 0) {
 }
 
 function createQueryArguments (args, userArgs) {
-  const queryArgs = []
-  args.forEach(arg => {
-    if (!userArgs[arg.name]) {
-      throw new Error(`${arg.name} is required and it is not defined`)
-    }
+  try {
+    const queryArgs = []
+    args.forEach(arg => {
+      if (!userArgs[arg.name]) {
+        throw new Error()
+      }
 
-    const selectedArg = userArgs[arg.name]
+      const selectedArg = userArgs[arg.name]
 
-    let userArg
+      let userArg
 
-    if (typeof selectedArg !== constants.boolean && typeof selectedArg !== constants.number) {
-      userArg = `"${userArgs[arg.name]}"`
-    } else {
-      userArg = selectedArg
-    }
+      if (typeof selectedArg !== constants.boolean && typeof selectedArg !== constants.number) {
+        userArg = `"${userArgs[arg.name]}"`
+      } else {
+        userArg = selectedArg
+      }
 
-    queryArgs.push(`${arg.name}: ${userArg}`)
-  })
+      queryArgs.push(`${arg.name}: ${userArg}`)
+    })
 
-  let test = queryArgs.join(', ')
-  test = test.replace(/"\[/g, '[')
-  test = test.replace(/\]"/g, ']')
+    let test = queryArgs.join(', ')
+    test = test.replace(/"\[/g, '[')
+    test = test.replace(/\]"/g, ']')
 
-  return test
+    return test
+  } catch (err) {
+    throw new Error('All query arguments must be defined')
+  }
 }
 
-module.exports = { getField, createQueryArguments }
+function createUnionQuery (nestedType, schema, queryName) {
+  const fields = []
+  nestedType.fields.forEach(field => {
+    const createdField = getField(field, schema, 2)
+    fields.push(createdField)
+  })
+  const unionQuery = `
+    ... on ${queryName} {
+      ${fields.join(' ')}
+    }
+  `
+
+  return unionQuery
+}
+
+function createQueryToTest (fields, queryHeader) {
+  const queryFields = fields.join(' ')
+
+  const newQuery = `
+    {
+      ${queryHeader} {
+        ${queryFields}
+      }
+    }
+  `
+
+  const queryToTest = {
+    name: queryHeader,
+    query: newQuery
+  }
+
+  return queryToTest
+}
+
+module.exports = { getField, createQueryArguments, createUnionQuery, createQueryToTest }
